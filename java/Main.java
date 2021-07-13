369eb538678780ecbbbf02b344e84867f8ed9c2b
Roberto Oliveira <rguimara@redhat.com>
2021-07-13 12:23:39 +0200

    add Singleton example with error

---
+++ java/Main.java
@@ -0,0 +1,31 @@
+
+
+public class Main {
+    public static void main(String args[]) {
+        JDBC jdbc;
+        for (int i = 0; i < 5; i++) {
+            jdbc = JDBC.getInstance();
+            System.out.println("Number of created objects = " + jdbc.getCount());
+        }
+
+    }
+}
+
+class JDBC {
+    private static JDBC instance;
+    private static int count = 0;
+
+    public JDBC() {
+        count++;
+    }
+
+    /** Lazy initialization */
+    public static JDBC getInstance() {
+        instance = new JDBC();
+        return instance;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
